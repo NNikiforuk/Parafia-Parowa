a11b48142095e0e90ba8ffeff7fffd5ce8cce0a1
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-12-23 16:34:07 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php blog_info(" charset "); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, a?</p>
+<h1>dfg</h1>
+</body>
+</html>
