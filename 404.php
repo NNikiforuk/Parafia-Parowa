08f953e9a43e64be1cb534ef635fb14b45cfc3f9
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-01-03 16:47:29 +0100

    404 page

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <div class="container">
+                <div class="error-404">
+                    <header>
+                        <h1>Page not found</h1>
+                        <p>Unfortunately, the page you tried to reach does not exist on this site.</p>
+                    </header>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+<?php get_footer(); ?>
