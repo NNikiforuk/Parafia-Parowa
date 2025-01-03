96fd64cfa3b580e3c136cb6d26396c7168a5f06f
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-01-03 16:58:25 +0100

    Reusable content

--- page.php
+++ page.php
@@ -6,13 +6,8 @@
                 <div class="page-item">
                     <?php
                     while (have_posts()) : the_post();
+                        get_template_part('parts/content', 'page');
                     ?>
-                        <article>
-                            <header>
-                                <h1><?php the_title(); ?></h1>
-                            </header>
-                            <?php the_content(); ?>
-                        </article>
                     <?php
                     endwhile;
                     ?>
