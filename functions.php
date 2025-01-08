bec450f44caa5602a988f938fe01d2983e69405c
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-01-08 19:49:09 +0100

    changing cache settings

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function wpdevs_load_scripts()
 {
-    wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_style('wpdevs-style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'), 'all');
 
     wp_enqueue_script('dropdown', get_template_directory_uri() . '/js/dropdown.js', array(), '1.0', true);
     wp_enqueue_script('copyingNumber', get_template_directory_uri() . '/js/copyingNumber.js', array(), '1.0', true);
