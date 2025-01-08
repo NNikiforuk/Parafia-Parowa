016a5e4dfe78cf1dd6e0d20cb9f074f63f799599
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-01-08 18:07:07 +0100

    Hiding navbar on mobile when a clicked

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,16 @@
 let dropdown = document.querySelector(".menu"),
-	submenu = document.querySelector(".sub-menu"),
 	buttonClick = document.querySelector(".check-button"),
 	hamburger = document.querySelector(".menu-icon");
+	menuLinks = document.querySelectorAll(".menu a");
+
 
 buttonClick.addEventListener("click", () => {
 	dropdown.classList.toggle("show-dropdown");
-	if (submenu) {
-		submenu.classList.toggle("show-dropdown");
-	}
 	hamburger.classList.toggle("animate-button");
+});
+
+menuLinks.forEach((link) => {
+	link.addEventListener("click", function () {
+		dropdown.classList.remove("show-dropdown")
+	});
 });
