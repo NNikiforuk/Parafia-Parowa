65c2bec335201cc1e4065b33f652511256fc2f98
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-01-08 14:34:03 +0100

    Single post styling

--- single.php
+++ single.php
@@ -1,19 +1,18 @@
 <?php get_header() ?>
-
 <div id="primary">
     <div id="main">
-        <div class="container">
-
-            <?php
-            while (have_posts()):
-                the_post();
-                get_template_part('parts/content', 'single');
-            ?>
-
-            <?php
-            endwhile;
-            ?>
-
+        <div class="single-post">
+            <header>
+                <h2><?php the_title(); ?></h2>
+            </header>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <div class="content">
+                    <?php the_content(); ?>
+                </div>
+            </article>
+            <div class="all-news">
+                <a class="all-news-link" href="http://localhost:8000/aktualnosci">Powrót do wszystkich aktualności</a>
+            </div>
         </div>
     </div>
 </div>
