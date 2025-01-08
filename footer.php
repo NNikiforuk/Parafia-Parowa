4ada69a678f80f19c1f63bf441c75c063e57c8a0
Natalia Nikiforuk <natnikiforuk@gmail.com>
2025-01-08 14:52:53 +0100

    Current year in footer

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 <footer class="site-footer">
     <div class="container">
-        <p>@2024 Parafia w Parowej</p>
+        <p>@2025 Parafia w Parowej</p>
         <p><a href="mailto:natnikiforuk@gmail.com">wykonanie: nnikiforuk</a></p>
     </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
-
 </html>
